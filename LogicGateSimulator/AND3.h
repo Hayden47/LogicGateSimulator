be4774215fc8b1121052522911fde9c852858d68
Oscar Reyes <OscarEReyes@users.noreply.github.com>
2016-04-24 15:10:02 -0500

    Update AND3.h

--- LogicGateSimulator/AND3.h
+++ LogicGateSimulator/AND3.h
@@ -1,28 +1,15 @@
 #pragma once
-#include"AND2.h"
-
-class AND3 : public gate {
-
-private:
-	int input[3];
-	gate *inputPointer[3];
-	int inputPointerField;
+#include"gate.h"
+#include"AND.h"
+class AND3 : public AND<3> {
 
 public:
+	AND3() {
+		for (int inp : input) {
+			inp = 2;				// sets each input value to 2
+		}
+		presentOutput = 2;
 
-	AND3::AND3();
+	}
 
-	virtual int AND3::evaluate();
-
-	void AND3::setInput(int, int);
-	void AND3::setInputPointer(int, gate *g);
-
-	void AND3::setOutputPointer(gate*);
-	gate* AND3::getOutputPointer();
-
-	void AND3::setOutputPointerField(int);
-	int AND3::getOutputPointerField();
-
-	void AND3::setPresentOutput(int);
-	int AND3::getPresentOutput();
 };
