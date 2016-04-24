16f4aacd9b87071534c29ca58c99e278f27d0514
Oscar Reyes <OscarEReyes@users.noreply.github.com>
2016-04-24 15:10:18 -0500

    Update AND4.h

--- LogicGateSimulator/AND4.h
+++ LogicGateSimulator/AND4.h
@@ -1,26 +1,15 @@
 #pragma once
 #include"gate.h"
-class AND4 : public gate {
-
-private:
-	int input[4];
-	gate *inputPointer[4];
-	int inputPointerField;
+#include"AND.h"
+class AND4 : public AND<4> {
 
 public:
-	AND4::AND4();
+	AND4() {
+		for (int inp : input) {
+			inp = 2;				// sets each input value to 2
+		}
+		presentOutput = 2;
 
-	int AND4::evaluate();
+	}
 
-	void AND4::setInput(int, int);
-	void AND4::setInputPointer(int, gate *g);
-
-	void AND4::setOutputPointer(gate*);
-	gate* AND4::getOutputPointer();
-
-	void AND4::setOutputPointerField(int);
-	int AND4::getOutputPointerField();
-
-	void AND4::setPresentOutput(int);
-	int AND4::getPresentOutput();
 };
