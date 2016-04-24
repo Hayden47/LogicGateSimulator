be673acf8b4772c1b581e63af506252533291a5c
Oscar Reyes <OscarEReyes@users.noreply.github.com>
2016-04-24 13:31:33 -0500

    Update OR4.h

--- LogicGateSimulator/OR4.h
+++ LogicGateSimulator/OR4.h
@@ -1,16 +1,15 @@
 #pragma once
-#include"OR2.h"
-
-
-class OR4 : public OR2 {
-
-
-private:
-	int input[4];
-	gate *inputPointer[4];
+#include"gate.h"
+#include"OR.h"
+class OR4 : public OR<4> {
 
 public:
+	OR4() {
+		for (int inp : input) {
+			inp = 2;				// sets each input value to 2
+		}
+		presentOutput = 2;
 
-	OR4();
+	}
 
 };
