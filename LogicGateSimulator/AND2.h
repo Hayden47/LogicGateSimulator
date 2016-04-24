571e43798b906ff72b2167be66294a0cfcbbf046
Oscar Reyes <OscarEReyes@users.noreply.github.com>
2016-04-24 15:09:46 -0500

    Update AND2.h

--- LogicGateSimulator/AND2.h
+++ LogicGateSimulator/AND2.h
@@ -1,32 +1,15 @@
 #pragma once
 #include"gate.h"
+#include"AND.h"
+class AND2 : public AND<2> {
 
-class AND2 : public gate {
+public:
+	AND2() {
+		for (int inp : input) {
+			inp = 2;				// sets each input value to 2
+		}
+		presentOutput = 2;
 
+	}
 
-
-private: 
-	int input[2];
-	gate *inputPointer[2];
-	int inputPointerField;
-
-public: 
-	
-	AND2::AND2();
-
-	virtual int AND2::evaluate();
-
-	void AND2::setInput(int,int);
-	void AND2::setInputPointer(int,gate *g);
-
-	void AND2::setOutputPointer(gate*);
-	gate* AND2::getOutputPointer();
-
-	void AND2::setOutputPointerField(int);
-	int AND2::getOutputPointerField();
-
-	void AND2::setPresentOutput(int);
-	int AND2::getPresentOutput();
-	
 };
-
