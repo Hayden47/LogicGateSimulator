6316ee4f3b3ef75f8a65d7fe897264ff7f7eafac
Oscar Reyes <OscarEReyes@users.noreply.github.com>
2016-04-27 14:03:48 -0500

    Added files via upload
    Add NOR Gates

---
+++ NOR3.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"gate.h"
+#include"NOR.h"
+class NOR3 : public NOR<3> {
+
+public:
+	NOR3() {
+		for (int inp : input) {
+			inp = 2;				// sets each input value to 2
+		}
+		presentOutput = 2;
+
+	}
+
+};
