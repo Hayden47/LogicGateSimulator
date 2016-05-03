43b1cbba55a9c0776beef7ff23b548db2f94abc7
Oscar Reyes <OscarEReyes@users.noreply.github.com>
2016-05-02 20:57:05 -0500

    Update BUFFER.cpp

--- LogicGateSimulator/BUFFER.cpp
+++ LogicGateSimulator/BUFFER.cpp
@@ -5,33 +5,38 @@ BUFFER::BUFFER() {
 	input[0] = 2;
 }
 
+// Evaluate function definition.
 int BUFFER::evaluate() {
 	return input[0];
 }
 
+// Sets input value
 void BUFFER::setInput(int inputNum, int value) {
 	input[inputNum] = value;
 }
 
-
+// Sets present output
 void BUFFER::setPresentOutput(int value) {
 	presentOutput = value;
 }
 
+// Sets input pointer
 void BUFFER::setInputPointer(int n, gate* g) {
 	inputPointer[n] = g;
 }
 
+// Sets input pointers
 void BUFFER::setInputPointers(std::vector<gate*> &list) {
 	inputPointer[0] = list[input[0]];
 	input[0] = 2;
 }
 
+// Gets the present output
 int BUFFER::getPresentOutput() {
 	return presentOutput;
 }
 
-
+// Checks for change in inputs and updates array when changes are found.
 bool BUFFER::inputHasChanged() {
 	bool hasChanged = false;
 	if (input[0] != inputPointer[0]->getPresentOutput()) {
