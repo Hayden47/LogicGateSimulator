ff1c22c90c9d1997eaff247fea2ed04fa7df6ad9
Oscar Reyes <OscarEReyes@users.noreply.github.com>
2016-05-02 21:12:07 -0500

    Update INPUT.cpp

--- LogicGateSimulator/INPUT.cpp
+++ LogicGateSimulator/INPUT.cpp
@@ -4,26 +4,34 @@ INPUT::INPUT() {
 	presentOutput = 2;
 	input[0] = 2;
 }
+
+// Evaluates 
 int INPUT::evaluate() {
 	return input[0];
 }
 
+// Sets input 
 void INPUT::setInput(int inputNum, int value) {
 	input[inputNum] = value;
 }
 
+// Sets the present output
 void INPUT::setPresentOutput(int value) {
 	presentOutput = value;
 
 }
 
-
+// Gets present output
 int INPUT::getPresentOutput() {
 	return presentOutput;
 }
+
+// Checks for change in inputs and updates array when changes are found.
 bool INPUT::inputHasChanged() {
 	return (presentOutput != input[0]);
 }
+
+// Returns true
 bool INPUT::isInput() {
 	return true;
 }
